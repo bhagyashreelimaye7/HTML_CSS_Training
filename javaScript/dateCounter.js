4d8292db02050e9e3bb72a86cf8f6f9aaf107ca5
Bhagyashree Limaye <Bhagyashree@Bhagyashrees-MacBook-Air.local>
2016-02-10 18:05:07 +0530

    INT-204 the date difference calculator pushed

---
+++ javaScript/dateCounter.js
@@ -0,0 +1,40 @@
+  function start(){
+    var currentDate = new Date();
+    var cmonth = currentDate.getMonth()+1;
+    var cday =currentDate.getDate();
+    var cyear = currentDate.getFullYear();
+    var ctime = currentDate.getTime();
+
+
+   var date = document.getElementById("date").value;
+   if (date) {
+
+   var bdate= new Date(date);
+   var bmonth = bdate.getMonth()+1;
+   var bday = bdate.getDate();
+   var byear = bdate.getFullYear();
+   var btime = bdate.getTime();
+
+
+   var timeDiff = Math.abs(currentDate.getTime() - bdate.getTime());
+   var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+   var diffHours = Math.ceil(Math.abs(currentDate - bdate) / 36e5);
+   var diffMin = diffHours*60;
+
+   document.getElementById("days").value= diffDays;
+   document.getElementById("hours").value= diffHours;
+   document.getElementById("min").value= diffMin;
+   }
+   else{
+    document.getElementById(err).value="Date cannot be blank";
+   }
+
+  }
+
+
+function reset(){
+  document.getElementById("date").value="";
+  document.getElementById("days").value="";
+  document.getElementById("hours").value="";
+  document.getElementById("min").value="";
+}
