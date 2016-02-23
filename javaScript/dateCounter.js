9ac9aabe8a15469b7f791a4392e6434596ac7974
Bhagyashree Limaye <Bhagyashree@Bhagyashrees-MacBook-Air.local>
2016-02-23 16:32:49 +0530

    INT-204 date files updated

--- javaScript/dateCounter.js
+++ javaScript/dateCounter.js
@@ -1,4 +1,11 @@
+(function($){
+  $(document).ready(function($) {
+    $("#datepicker").datepicker();
+  });
+})(jQuery);
+
   function start(){
+
     var currentDate = new Date();
     var cmonth = currentDate.getMonth()+1;
     var cday =currentDate.getDate();
@@ -6,10 +13,11 @@
     var ctime = currentDate.getTime();
 
 
-   var date = document.getElementById("date").value;
+   var date = document.getElementById("datepicker").value;
    if (date) {
 
    var bdate= new Date(date);
+   alert(bdate);
    var bmonth = bdate.getMonth()+1;
    var bday = bdate.getDate();
    var byear = bdate.getFullYear();
@@ -18,12 +26,12 @@
 
    var timeDiff = Math.abs(currentDate.getTime() - bdate.getTime());
    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-   var diffHours = Math.ceil(Math.abs(currentDate - bdate) / 36e5);
+   var diffHours = Math.ceil(Math.abs(currentDate - bdate) / 365);
    var diffMin = diffHours*60;
 
    document.getElementById("days").value= diffDays;
-   document.getElementById("hours").value= diffHours;
-   document.getElementById("min").value= diffMin;
+   document.getElementById("hours").value= currentDate.getHours();
+   document.getElementById("min").value= currentDate.getMinutes();
    document.getElementById('err').innerHTML = "";
    }
    else{
@@ -34,7 +42,7 @@
 
 
 function reset(){
-  document.getElementById("date").value="";
+  document.getElementById("datepicker").value="";
   document.getElementById("days").value="";
   document.getElementById("hours").value="";
   document.getElementById("min").value="";
